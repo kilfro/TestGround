6c620ba03262f2acec7db8998a703af7e15e6c6d
Kirill Frolov <frolov.cyrill@gmail.com>
2020-04-28 15:32:55 +0300

    Добавил Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from './components/pages/Login';
+import NotFound from "./components/pages/NotFound";
 
 function App() {
-  return (
-    <div className="App">
-        <Login/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Switch>
+                    <Route path={'/login'} component={Login}/>
+                    <Route path={'*'} component={NotFound}/>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
