70d4857dc0e878f4951a81b91de7ef822e6104ad
Kirill Frolov <frolov.cyrill@gmail.com>
2020-04-29 19:23:27 +0300

    Отображение ошибок

--- src/App.js
+++ src/App.js
@@ -6,8 +6,14 @@ import Login from './components/pages/Login';
 import NotFound from "./components/pages/NotFound";
 import Registration from "./components/pages/Registration";
 import HomePage from "./components/pages/HomePage";
+import {Snackbar} from "@material-ui/core";
+import {Alert} from '@material-ui/lab';
+import {connect} from 'react-redux';
+import {deleteError} from "./store/actions/error";
 
-const App = () => {
+const App = (props) => {
+    const {error, deleteError} = props;
+
     return (
         <div className="App">
             <BrowserRouter>
@@ -18,8 +24,24 @@ const App = () => {
                     <Route path={'*'} component={NotFound}/>
                 </Switch>
             </BrowserRouter>
+
+            <Snackbar open={error} autoHideDuration={6000} onClose={deleteError}>
+                <Alert onClose={deleteError} severity="error">
+                    {error}
+                </Alert>
+            </Snackbar>
         </div>
     );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+    return {
+        error: state.error
+    }
+};
+
+const mapDispatchToProps = {
+    deleteError
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
