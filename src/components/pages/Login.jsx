f0246087a978b3358cf2de684efb1d2790758a76
Kirill Frolov <frolov.cyrill@gmail.com>
2020-05-01 19:45:56 +0300

    Тест событий в Login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -6,32 +6,56 @@ import '../../styles/component/pages/login.css';
 import {Redirect} from "react-router-dom";
 import logo from '../../logo.png';
 
-class Login extends React.Component {
+export class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+    }
+
+    onChangeHandler = (event) => {
+        const {id, value} = event.target;
+        this.setState({[id]: value});
+    };
+
     submitEmailLogin = (event) => {
         event.preventDefault();
-
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
-
+        const {email, password} = this.state;
         this.props.loginWithEmail(email, password);
     };
 
     render() {
         const {authenticated, loginWithGoogle} = this.props;
+        const {email, password} = this.state;
+
         return (
             <Container className={'login-form-container'} maxWidth={'xs'}>
                 {authenticated && <Redirect to={'/'}/>}
                 <img src={logo} alt={'logo'}/>
                 <h3>Войдите в Ваш TestGround</h3>
                 <form onSubmit={this.submitEmailLogin}>
-                    <TextField id={'email'} label={'E-mail'} type={'email'} fullWidth/>
-                    <TextField id={'password'} label={'Пароль'} type={'password'} fullWidth/>
+                    <TextField id={'email'}
+                               label={'E-mail'}
+                               type={'email'}
+                               fullWidth
+                               value={email}
+                               onChange={this.onChangeHandler}
+                    />
+                    <TextField id={'password'}
+                               label={'Пароль'}
+                               type={'password'}
+                               fullWidth
+                               value={password}
+                               onChange={this.onChangeHandler}
+                    />
                     <Button id={'login-btn'} type={'submit'} color={'primary'} variant={'contained'}
                             fullWidth className={'ground_btn'}>Войти</Button>
                     <Button href={'/registration'} fullWidth>Зарегистрироваться</Button>
                 </form>
                 <h4>или</h4>
-                <Button size={'large'} variant="outlined" onClick={() => loginWithGoogle()}>
+                <Button id={'google-btn'} size={'large'} variant="outlined" onClick={() => loginWithGoogle()}>
                     <img className={'google_logo'}
                          src={'https://www.gstatic.com/mobilesdk/160512_mobilesdk/auth_service_google.svg'}
                          alt={'google logo'}/>
--- test/components/pages/Login.test.js
+++ test/components/pages/Login.test.js
@@ -1,23 +1,64 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import {act, create} from 'react-test-renderer';
 import configureStore from 'redux-mock-store';
 import Login from "../../../src/components/pages/Login";
 import {Provider} from "react-redux";
+import {AUTH} from "../../../src/store/actionTypes";
+import {Router} from "react-router-dom";
 
 const mockStore = configureStore([]);
 
 describe('Login page', () => {
-    it('should be correctly', () => {
-        const store = mockStore({
+    let store;
+    let component;
+
+    beforeEach(() => {
+        store = mockStore({
             auth: {
                 authenticated: false
             }
         });
-        const tree = renderer.create(
+
+        store.dispatch = jest.fn();
+
+        component = create(
             <Provider store={store}>
                 <Login/>
             </Provider>
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
+        );
+    });
+
+    test('should be correctly', () => {
+        expect(component.toJSON())
+            .toMatchSnapshot();
+    });
+
+    test('should dispatch an action on google button click', () => {
+        act(() => {
+            component.root.findByProps({id: 'google-btn'}).props.onClick();
+        });
+
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledWith({type: AUTH.LOGIN_WITH_GOOGLE});
+    });
+
+    test('should dispatch an action on login button click', () => {
+        const root = component.root;
+        const event = {
+            preventDefault: () => {
+            }
+        };
+        act(() => {
+            root.findByType('form').props.onSubmit(event);
+        });
+
+        expect(store.dispatch).toHaveBeenCalledTimes(1);
+        expect(store.dispatch).toHaveBeenCalledWith({
+            type: AUTH.LOGIN_WITH_EMAIL,
+            payload: {
+                email: '',
+                password: ''
+            }
+        });
     });
 });
