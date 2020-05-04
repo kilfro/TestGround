59af28f5576227bc11940db7d82222c5281b9848
Kirill Frolov <frolov.cyrill@gmail.com>
2020-05-04 14:24:59 +0300

    Тесты action creators

---
+++ test/components/store/actions/Auth.test.js
@@ -0,0 +1,54 @@
+import {
+    loginSuccess,
+    loginWithEmail,
+    loginWithGoogle,
+    logoutRequest,
+    logoutSuccess
+} from '../../../../src/store/actions/auth';
+import {AUTH} from '../../../../src/store/actionTypes';
+
+describe('Auth action creator', () => {
+    test('login with google action is created correctly', () => {
+        expect(loginWithGoogle())
+            .toEqual({
+                type: AUTH.LOGIN_WITH_GOOGLE
+            });
+    });
+
+    test('login with email action is created correctly', () => {
+        expect(loginWithEmail('email', 'password'))
+            .toEqual({
+                type: AUTH.LOGIN_WITH_EMAIL,
+                payload: {
+                    email: 'email',
+                    password: 'password'
+                }
+            })
+    });
+
+    test('login success action is created correctly', () => {
+        expect(loginSuccess({name: 'UserName'}, 'token'))
+            .toEqual({
+                type: AUTH.LOGIN_SUCCESS,
+                payload: {
+                    token: 'token',
+                    user: {name: 'UserName'}
+                }
+            })
+    });
+
+    test('logout request action is created correctly', () => {
+        expect(logoutRequest())
+            .toEqual({
+                type: AUTH.LOGOUT_REQUEST
+            })
+    });
+
+    test('logout success action is created correctly', () => {
+        expect(logoutSuccess())
+            .toEqual({
+                type: AUTH.LOGOUT_SUCCESS
+            })
+    });
+});
+
---
+++ test/components/store/actions/Error.test.js
@@ -0,0 +1,19 @@
+import {createError, deleteError} from '../../../../src/store/actions/error';
+import {ERROR} from '../../../../src/store/actionTypes';
+
+describe('Error action creator', () => {
+    test('create error action works correctly', () => {
+        expect(createError('error message'))
+            .toEqual({
+                type: ERROR.CREATE,
+                payload: 'error message'
+            });
+    });
+
+    test('delete error action works correctly', () => {
+        expect(deleteError())
+            .toEqual({
+                type: ERROR.DELETE
+            })
+    });
+});
