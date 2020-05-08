9944a3b28966e4e7e610dd9ca634c529332d3bb4
Kirill Frolov <frolov.cyrill@gmail.com>
2020-05-08 18:42:29 +0300

    TG-4 Подключение QuestionList к Redux

--- src/components/creator/QuestionsList.jsx
+++ src/components/creator/QuestionsList.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import {Button} from "@material-ui/core";
 import Question from "./Question";
+import {connect} from 'react-redux';
+import {addQuestion} from "../../store/actions/newTest";
+import initialState from "../../store/initialState";
+import '../../styles/component/creator/questions-list.css';
 
 class QuestionsList extends React.Component {
+    createNewQuestion = () => {
+        const {questions, addQuestion} = this.props;
+
+        const id = questions.length + 1;
+        const newQuestion = {
+            ...initialState.newTest.questions[0],
+            id
+        };
+
+        addQuestion(newQuestion);
+    };
+
     render() {
-        const {questions = [], createNewQuestion} = this.props;
+        const {questions} = this.props;
 
         return (
             <>
-                {questions.map(q => <Question question={q}/>)}
-                <Button fullWidth id={'add_btn'} color={'primary'} variant={'contained'} onClick={createNewQuestion}>
+                {questions.map(q => <Question question={q} key={q.id}/>)}
+                <Button fullWidth id={'add_btn'} color={'primary'} variant={'contained'}
+                        onClick={this.createNewQuestion}>
                     Добавить вопрос
                 </Button>
             </>
@@ -17,4 +34,14 @@ class QuestionsList extends React.Component {
     }
 }
 
-export default QuestionsList;
+const mapStateToProps = (state) => {
+    return {
+        questions: state.newTest.questions
+    }
+};
+
+const mapDispatchToProps = {
+    addQuestion
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuestionsList);
