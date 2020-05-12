e6ee32558bb9ee5e600566f2ad846f6a66a7a6e7
Kirill Frolov <frolov.cyrill@gmail.com>
2020-05-12 14:17:50 +0300

    TG-4 Тест RadioOption

--- test/components/creator/RadioOption.test.js
+++ test/components/creator/RadioOption.test.js
@@ -12,8 +12,10 @@ describe('RadioOption', () => {
 
     const wrapper = mount(<RadioOption option={option} handleChange={handleFunc} remove={removeFunc}/>);
 
-    it('should render correctly', () => {
-        expect(wrapper).toMatchSnapshot();
+    it('should render radio', () => {
+        const radio = wrapper.find('input[id="isRight.1"]');
+        expect(radio.length).toEqual(1);
+        expect(radio.props().type).toEqual('radio');
     });
 
     it('should show option text', () => {
